6fb3b988b1ea26cc7e0851989fd326974bebf545
Lucas <berrylucas333@gmail.com>
2025-10-14 09:52:25 +0200

    commit

--- PageDeJeu.py
+++ PageDeJeu.py
@@ -1,6 +1,8 @@
 import tkinter as tk
+from classes import Brique
 
 class PageJeu(tk.Frame):
+    '''Classe représentant la page de jeu'''
     def __init__(self, parent, controller):
         super().__init__(parent)
         self.controller = controller
@@ -8,11 +10,8 @@ class PageJeu(tk.Frame):
         label = tk.Label(self, text="Page du jeu")
         label.pack(pady=20)
 
-        canvas = tk.Canvas(self, width=1280, height=720, bg="black")
-        canvas.pack(pady=10)
+        canvasDeJeu = tk.Canvas(self, width=720, height=480, bg="black")
+        canvasDeJeu.pack(pady=10)
 
         boutonRetour = tk.Button(self, text="Retour à l'accueil",command=lambda: controller.show_frame("PageAccueil"))
         boutonRetour.pack()
-        
-
-
