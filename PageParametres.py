dbc041d14a8bcb00d44878a852b67fe30e6f547f
Lucas <berrylucas333@gmail.com>
2025-10-14 10:40:17 +0200

    raquette

--- PageParametres.py
+++ PageParametres.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 class PageParametres(tk.Frame):
     def __init__(self, parent, controller):
+        '''Cette page de parametres permettra de régler les paramètres du jeu en tant qu'utilisateur'''
         super().__init__(parent)
         self.controller = controller
 
@@ -10,6 +11,19 @@ class PageParametres(tk.Frame):
 
         canvas = tk.Canvas(self, width=200, height=100, bg="lightblue")
         canvas.pack(pady=10)
+
+
+        #affiche ce que l'utilisateur a entré dans vitesse
+        def afficherScore(Str):
+            score = vitesse.get()
+            score_label = tk.Label(self, text=f"Score: {score}", font=("Arial", 16))
+            score_label.pack(pady=10, side=tk.TOP)
+
+        vitesse = tk.Entry(self) #champ de texte pour que l'utilisateur puisse entrer la vitesse de la balle
+        vitesse.pack()
+
+        validerVitesse = tk.Button(self, text="Valider la vitesse", command = afficherScore(vitesse)) #bouton pour valider la vitesse entrée par l'utilisateur
+        validerVitesse.pack()
 
         bouton_retour = tk.Button(self, text="Retour à l'accueil",command=lambda: controller.show_frame("PageAccueil"))
         bouton_retour.pack()
--- classes.py
+++ classes.py
@@ -53,3 +53,7 @@ class Raquette:
         self.__couleur = (0, 0, 255)
         self.__position = [250, 450]
         self.__vitesse = 10
+        def getX(self):
+            return self.__position[0] # la raquette se déplace en 1 dimension (x)
+        def addX(self, dx):
+            self.__position[0] += dx
