0d15a04238f746d5581b631d0f11d95a2035a41f
Lucas <berrylucas333@gmail.com>
2025-10-21 13:38:36 +0200

    classe brique complete

--- classeBrique.py
+++ classeBrique.py
@@ -1,16 +1,11 @@
-from foncitons import rgb
-
 class Brique:
-    def __init__(self):
-        """Initialise une brique avec ses caractéristiques par défaut."""
-        self.__largeur = 75
-        self.__hauteur = 20
-        self.__couleur = (['#FF3D17', ])     # Rouge
-        self.__position = [0, 0]         # Position (coin haut-gauche)
-        self.__visible = True            # Sert à la masquer quand elle est cassée
-        self.__vie = 2                   # Nombre de coups avant de disparaître
-        self.__id = None                 # ID de l'objet Canvas une fois affiché
+    def __init__(self, canvas, x1, y1, x2, y2, color="orange", hits=1):
+        self.canvas = canvas
+        self.hits = hits
+        self.id = canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="black")
 
-    def set_position(self, x, y):
-        """Définit la position de la brique sur le Canvas."""
-        self.__position = [x, y]
+    def destroy(self):
+        try:
+            self.canvas.delete(self.id)
+        except Exception:
+            pass
