cf3365418240580d90197341e26336ed490620d6
Lucas <berrylucas333@gmail.com>
2025-10-28 13:51:40 +0100

    commentaires sur main

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+'''
+Cree par Lucas BERRY et Baudry DEROUBAIX modifié en dernier le 22/10/25
+But : lancer le programme principal du jeu de casse-briques.
+'''
+
 import tkinter as tk
 from ProgrammeJeu import JeuCasseBrique
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    game = JeuCasseBrique(root)
-    root.mainloop()
+    root = tk.Tk() # création de la fenêtre principale
+    game = JeuCasseBrique(root) # création de l'objet jeu
+    root.mainloop() # boucle principale de la fenêtre
